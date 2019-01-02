13c38c370c99542fcfbd84cf16401dec9199e930
iq-mschmeets <mark@inquisient.com>
2019-01-02 08:58:19 -0500

    Notes

--- src/BusinessCapabilityBPMNDiagramList.js
+++ src/BusinessCapabilityBPMNDiagramList.js
@@ -5,13 +5,17 @@ import BusinessCapability from "./BusinessCapability.js";
 
 export default function BusinessCapabilityBPMNDiagramList(props) {
 	return (
-		<ul style={{ paddingLeft: 10 }} className="list-unstyled">
-			{Object.keys(props.data).map(function(key) {
+		<ul
+			style={{ paddingLeft: 0, borderTop: "1px solid #f2f2f2" }}
+			className="list-unstyled"
+		>
+			{Object.keys(props.data).map(function(key, indx) {
 				let rows = props.data[key];
 
 				return (
 					<BusinessCapability
 						label={key}
+						key={indx}
 						rows={rows}
 						onRemoveDiagram={props.onRemoveDiagram}
 						onAddDiagram={props.onAddDiagram}
